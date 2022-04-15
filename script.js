b0dc652ee34d30004dd9420924b95984f34e1c24
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-04-14 22:14:51 -0300

    Dando continuidade ao script

--- script.js
+++ script.js
@@ -60,3 +60,13 @@ c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
         c('.pizzaInfo--qt').innerHTML = modalQt;
     }
 });
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
