4cc54b68a6935944db92a5e880c33ec67961dd36
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-04-15 23:26:37 -0300

    Dando continuidade ao addButton

--- script.js
+++ script.js
@@ -70,3 +70,20 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
         size.classList.add('selected');
     });
 });
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+    let identifier = pizzaJson[modalKey].id+'@'+size;
+    let key = cart.findIndex((item)=>item.identifier == identifier);
+    if(key > -1) {
+        cart[key].qt += modalQt;
+    } else {
+        cart.push({
+            identifier,
+            id:pizzaJson[modalKey].id,
+            size,
+            qt:modalQt
+        });
+    }
+    updateCart();
+    closeModal();
+});
