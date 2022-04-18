ede04b92f2f5abf59a72f8869d9f3554614197ef
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-04-17 23:36:02 -0300

    Criando o primeiro ID

--- pizzas.js
+++ pizzas.js
@@ -1,5 +1,6 @@
 let pizzaJson = [
-
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    
 
 
 
