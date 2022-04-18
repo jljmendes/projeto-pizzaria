c9f396469a2d4499a2255b4b6b68c6ccd4f66cd1
Jorge Mendes <jljmendesmanutencao@gmail.com>
2022-04-17 23:36:51 -0300

    Criando o segundo ID

--- pizzas.js
+++ pizzas.js
@@ -1,6 +1,6 @@
 let pizzaJson = [
     {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
 
 
 
